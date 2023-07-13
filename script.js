0ee74c0567074584aaf8ce7a6615318006a5fc33
Abel <abelgoshime.1@gmail.com>
2023-07-13 15:39:19 -0400

    bg Image change, current weather info

--- script.js
+++ script.js
@@ -8,24 +8,84 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-class API {
-    constructor() {
-        this.API_KEY = 'a04e68b564fb4838931192929231107';
-    }
-    getAPI_KEY() {
-        return this.API_KEY;
-    }
-    setAPI_KEY(API_KEY) {
-        this.API_KEY = API_KEY;
-    }
-}
 function getWeather(city) {
     return __awaiter(this, void 0, void 0, function* () {
-        const api = new API();
-        const url = `http://api.weatherapi.com/v1/current.json?key=${api.getAPI_KEY()}&q=${city}`;
-        const response = yield fetch(url);
+        const api = "a04e68b564fb4838931192929231107";
+        const url = `http://api.weatherapi.com/v1/current.json?key=${api}&q=${city}&aqi=yes`;
+        const response = yield fetch(url, { mode: "cors" });
         const data = yield response.json();
         return data;
     });
 }
-//___________________________________________________________________________________//
+function addWeatherInfo(data) {
+    const weather = document.querySelector(".weather");
+    const weatherData = `
+    <div class="weather">
+  <div class="weatherCard">
+    <div class="weather__info">
+      <div class="weather__temp">${data.current.temp_f}°F</div>
+      <div class="weather__desc">${data.current.condition.text}</div>
+
+      <div class="weather__city">
+        <h5 class="weather__city-name">${data.location.name}, ${data.location.country}</h5>
+        <div class="weather__city-date">${data.location.localtime}</div>
+        <p class="feels-like">Feels like ${data.current.feelslike_f}°F</p>
+      </div>
+    </div>
+    <div class="weather-icon">
+      <img src="${data.current.condition.icon}" alt="${data.current.condition.text}" />
+    </div>
+  </div>
+    `;
+    weather.innerHTML = weatherData;
+    changeBackgroundImg(data);
+}
+const searchBtn = document.querySelector(".search-btn");
+const searchInput = document.querySelector(".search-bar");
+const searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const city = searchInput.value;
+    getWeather(city)
+        .then((data) => {
+        addWeatherInfo(data);
+    })
+        .catch(() => {
+        const weather = document.querySelector(".weather");
+        weather.innerHTML = `<h2 class="weather__city-name">Please enter a valid city</h2>`;
+    });
+    searchInput.value = "";
+});
+//_______________________________Change Background Img____________________________________________________//
+const sunnyBackground = new Image();
+sunnyBackground.src = "/background-img/sun-rising-rock-formation-covered-greenery.jpg";
+const cloudySkyBackground = new Image();
+cloudySkyBackground.src = "/background-img/clouds-sky-vertical-shot.jpg";
+const rainySkyBackground = new Image();
+rainySkyBackground.src = "/background-img/weather-effects-composition.jpg";
+const snowySkyBackground = new Image();
+snowySkyBackground.src = "/background-img/sunbeam-through-snowy-forest.jpg";
+const defaultSkyBackground = new Image();
+defaultSkyBackground.src = "/background-img/peaceful-meadow-with-trees-distance.jpg";
+function changeBackgroundImg(data) {
+    //background change
+    const body = document.querySelector("body");
+    const condition = data.current.condition.text.toLowerCase();
+    let backgroundImage;
+    if (condition.includes("sunny")) {
+        backgroundImage = `url(${sunnyBackground.src})`;
+    }
+    else if (condition.includes("clouds")) {
+        backgroundImage = `url(${cloudySkyBackground.src})`;
+    }
+    else if (condition.includes("rain")) {
+        backgroundImage = `url(${rainySkyBackground.src})`;
+    }
+    else if (condition.includes("snow")) {
+        backgroundImage = `url(${snowySkyBackground.src})`;
+    }
+    else {
+        backgroundImage = `url(${defaultSkyBackground.src})`;
+    }
+    document.body.style.backgroundImage = backgroundImage;
+}
--- script.ts
+++ script.ts
@@ -1,45 +1,87 @@
 //__________________________________________Weather API Config__________________________________________//
 
-async function getWeather(city){
-    const api: string = 'a04e68b564fb4838931192929231107';
-    const url = `http://api.weatherapi.com/v1/current.json?key=${api}&q=${city}`;
-    const response = await fetch(url, {mode: 'cors'});
-    const data = await response.json();
-    return data;
+async function getWeather(city) {
+  const api: string = "a04e68b564fb4838931192929231107";
+  const url = `http://api.weatherapi.com/v1/current.json?key=${api}&q=${city}&aqi=yes`;
+  const response = await fetch(url, { mode: "cors" });
+  const data = await response.json();
+  return data;
 }
 
-const searchBtn = document.querySelector('.search-btn');
-const searchInput: any = document.querySelector('.search-input');
+function addWeatherInfo(data) {
+  const weather = document.querySelector(".weather");
+  const weatherData = `
+    <div class="weather">
+  <div class="weatherCard">
+    <div class="weather__info">
+      <div class="weather__temp">${data.current.temp_f}°F</div>
+      <div class="weather__desc">${data.current.condition.text}</div>
 
-searchBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const city = searchInput.value;
-    getWeather(city).then(data => {
-        console.log(data);
-        const weather = document.querySelector('.weather');
-        const weatherData = `
-        <div class="weather__city">
-            <h2 class="weather__city-name">${data.location.name}</h2>
-            <div class="weather__city-date">${data.location.localtime}</div>
-        </div>
-        <div class="weather__info">
-            <div class="weather__temp">${data.current.temp_c}°C</div>
-            <div class="weather__desc">${data.current.condition.text}</div>
+      <div class="weather__city">
+        <h5 class="weather__city-name">${data.location.name}, ${data.location.country}</h5>
+        <div class="weather__city-date">${data.location.localtime}</div>
+        <p class="feels-like">Feels like ${data.current.feelslike_f}°F</p>
+      </div>
+    </div>
+    <div class="weather-icon">
+      <img src="${data.current.condition.icon}" alt="${data.current.condition.text}" />
+    </div>
+  </div>
+    `;
+  weather.innerHTML = weatherData;
 
-        </div>
-        <div class="weather__icon">
-            <img src="${data.current.condition.icon}" alt="">
-        </div>
-        `;
-        weather.innerHTML = weatherData;
+  changeBackgroundImg(data);
+}
+
+const searchBtn = document.querySelector(".search-btn") as HTMLButtonElement;
+const searchInput: any = document.querySelector(".search-bar");
+const searchForm = document.querySelector(".search-form") as HTMLFormElement;
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const city = searchInput.value;
+  getWeather(city)
+    .then((data) => {
+      addWeatherInfo(data);
     })
     .catch(() => {
-        const weather = document.querySelector('.weather');
-        weather.innerHTML = `<h2 class="weather__city-name">Please enter a valid city</h2>`;
-    })
-})
+      const weather = document.querySelector(".weather");
+      weather.innerHTML = `<h2 class="weather__city-name">Please enter a valid city</h2>`;
+    });
+  searchInput.value = "";
+});
 
+//_______________________________Change Background Img____________________________________________________//
+const sunnyBackground = new Image();
+sunnyBackground.src = "/background-img/sun-rising-rock-formation-covered-greenery.jpg";
 
+const cloudySkyBackground = new Image();
+cloudySkyBackground.src = "/background-img/clouds-sky-vertical-shot.jpg";
 
-//___________________________________________________________________________________//
+const rainySkyBackground = new Image();
+rainySkyBackground.src = "/background-img/weather-effects-composition.jpg";
 
+const snowySkyBackground = new Image();
+snowySkyBackground.src = "/background-img/sunbeam-through-snowy-forest.jpg";
+
+const defaultSkyBackground = new Image();
+defaultSkyBackground.src = "/background-img/peaceful-meadow-with-trees-distance.jpg";
+
+function changeBackgroundImg(data) {
+    //background change
+    const body = document.querySelector("body");
+    const condition = data.current.condition.text.toLowerCase();
+    let backgroundImage: string;
+
+  if (condition.includes("sunny")) {
+    backgroundImage = `url(${sunnyBackground.src})`;
+  } else if (condition.includes("clouds")) {
+    backgroundImage = `url(${cloudySkyBackground.src})`;
+  } else if (condition.includes("rain")) {
+    backgroundImage = `url(${rainySkyBackground.src})`;
+  } else if (condition.includes("snow")) {
+    backgroundImage = `url(${snowySkyBackground.src})`;
+  } else {
+    backgroundImage = `url(${defaultSkyBackground.src})`;
+  }
+  document.body.style.backgroundImage = backgroundImage;
+}
